d602c4ac4123efd0a80bfa067087ed770a169003
nguyen34 <nguyenjohnson65@gmail.com>
2023-06-13 15:36:31 -0700

    Added routing/restructured frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./css/index.css";
 
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
@@ -10,6 +9,7 @@ import reducer from "./store/index";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const store = configureStore({ reducer: reducer, middleware: [thunk] });
 
@@ -17,11 +17,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/pages/Articles.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { Article } from "../components/Article";
+import { AddArticle } from "../components/AddArticle";
+import {
+  addArticle,
+  removeArticle,
+} from "../store/reducers/articles/actionCreators";
+import { getGreeting } from "../store/reducers/landing/actionCreators";
+import { Dispatch } from "redux";
+import axios from "axios";
+
+const Articles: React.FC = () => {
+  const articles = useSelector((state: any) => state.articles.articles);
+  const greeting = useSelector((state: any) => state.landing.greeting);
+
+  const dispatch: Dispatch<any> = useDispatch();
+
+  const saveArticle = React.useCallback(
+    (article: IArticle) => dispatch(addArticle(article)),
+    [dispatch]
+  );
+
+  function fetchArticles() {
+    axios.get("/api/articles").then((res) => {
+      const articles: IArticle[] = res.data.map((article: any) => {
+        return {
+          id: article.id,
+          title: article.title,
+          body: article.body,
+        };
+      });
+      articles.forEach((article) => saveArticle(article));
+    });
+  }
+
+  React.useEffect(() => {
+    dispatch(getGreeting());
+  });
+
+  return (
+    <main>
+      <h1>{greeting}</h1>
+      <AddArticle saveArticle={saveArticle} />
+      {articles.map((article: IArticle) => (
+        <Article
+          key={article.id}
+          article={article}
+          removeArticle={removeArticle}
+        />
+      ))}
+      <button onClick={fetchArticles}>Fetch Articles</button>
+    </main>
+  );
+};
+
+export default Articles;
